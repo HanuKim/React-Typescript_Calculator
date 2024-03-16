a682db79ff939d40a2ebe5d8d44fc1459f84ff20
lomen-KimHanUl <aouoa03@lomen.kr>
2024-03-17 02:01:38 +0900

    [REFACTOR] Complete Calculating feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,63 +2,96 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const [firstNumber, setFirstNumber] = useState(0);
-
-  const [secondNumber, setSecondNumber] = useState(0);
+  const [display, setDisplay] = useState(0);
+  const [memory, setMemory] = useState(0);
+  const [operator, setOperator] = useState("");
+  const [error, setError] = useState(false);
 
   const numbers: number[] = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
   const operators: string[] = ["/", "X", "-", "+", "="];
 
+  const calculate = (a: number, b: number, operator: string) => {
+    const operations: { [key: string]: (a: number, b: number) => number } = {
+      "+": (a, b) => a + b,
+      "-": (a, b) => a - b,
+      X: (a, b) => a * b,
+      "/": (a, b) => a / b,
+    };
+
+    const result = operations[operator](a, b);
+    if (!isFinite(result)) {
+      // 무한대 또는 계산 불가능한 결과인 경우 체크
+      setError(true);
+      return 0; // 상태의 타입에 맞추기 위해 0을 반환
+    }
+
+    return Math.floor(result);
+  };
+
   const handleAllClear = () => {
-    setCount(0);
-    setFirstNumber(0);
-    setSecondNumber(0);
+    setDisplay(0);
+    setMemory(0);
   };
 
   const handleNumberClick = (number: number) => {
-    if (count === 0) {
-      setCount(number);
-    } else if (count > 0 && count < 10) {
-      setCount(count * 10 + number);
-    } else if (count > 10 && count < 100) {
-      setCount(count * 10 + number);
-    } else {
-      alert("You can't enter more than 3 digits");
+    if (error) {
+      setError(false);
+      setDisplay(number);
     }
-  };
-
-  const handleOperatorClick = (operators: string) => {
-    if (!operators[4] && firstNumber === 0 && secondNumber === 0) {
-      setFirstNumber(count);
-      setCount(0);
-    } else if (!operators[4] && firstNumber !== 0 && secondNumber === 0) {
-      setSecondNumber(count);
-    } else if (!operators[4] && firstNumber !== 0 && secondNumber !== 0) {
-      alert("You only can calculate two numbers at a time");
-    } else if (operators[4] && firstNumber !== 0 && secondNumber !== 0) {
-      if (operators[0]) {
-        setCount(firstNumber / secondNumber);
-      } else if (operators[1]) {
-        setCount(firstNumber * secondNumber);
-      } else if (operators[2]) {
-        setCount(firstNumber - secondNumber);
-      } else if (operators[3]) {
-        setCount(firstNumber + secondNumber);
+    if (memory === 0) {
+      if (display === 0) {
+        setDisplay(number);
+      } else if (display > 0 && display < 10) {
+        setDisplay(display * 10 + number);
+      } else if (display > 10 && display < 100) {
+        setDisplay(display * 10 + number);
       } else {
-        alert("You can't calculate");
+        alert("You can't enter more than 3 digits");
+      }
+    } else {
+      setDisplay(0);
+      if (display === 0) {
+        setDisplay(number);
+      } else if (display > 0 && display < 10) {
+        setDisplay(display * 10 + number);
+      } else if (display > 10 && display < 100) {
+        setDisplay(display * 10 + number);
+      } else {
+        alert("You can't enter more than 3 digits");
+        setDisplay(display);
       }
     }
   };
 
-  console.log(count, firstNumber, secondNumber);
+  const handleOperatorClick = (operators: string) => {
+    if (operators === "=") {
+      if (memory === 0 || !operator) {
+        return;
+      }
+      setError(false);
+      const result = calculate(memory, display, operator);
+      setDisplay(result);
+      setMemory(0);
+      setOperator("");
+    } else {
+      if (memory !== 0 && operator !== "" && display !== 0) {
+        alert("You can calculate only two numbers at a time");
+        return;
+      }
+      setError(false);
+      setMemory(display);
+      setOperator(operators);
+      setDisplay(0);
+    }
+  };
+
+  console.log(display, memory, operator);
   return (
     <>
       <div className="wrapper">
         <div className="counter">
-          <p>{count}</p>
+          <p>{error ? "Error" : display}</p>
         </div>
         <div className="button-wrapper">
           <div className="left-side">
